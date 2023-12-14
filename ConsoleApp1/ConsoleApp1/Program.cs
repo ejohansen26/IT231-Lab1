e745cf06f5a226084f81f62c796d6e44995ea75c
ejohansen26 <112412176+ejohansen26@users.noreply.github.com>
2023-12-14 18:20:01 -0500

    Second commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,6 +5,11 @@
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello, World!");
+
+			Console.Write("Enter 1st number: ");
+			int num1 = Int32.Parse(Console.ReadLine());
+			Console.Write("Enter 2nd number: ");
+			int num2 = Int32.Parse(Console.ReadLine());
 		}
 	}
 }
