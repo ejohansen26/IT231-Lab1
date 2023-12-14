2d1f3215a1870daee147109fa0200d6e4acfadca
ejohansen26 <112412176+ejohansen26@users.noreply.github.com>
2023-12-14 18:25:19 -0500

    Multiplication commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,9 @@
 			int num1 = Int32.Parse(Console.ReadLine());
 			Console.Write("Enter 2nd number: ");
 			int num2 = Int32.Parse(Console.ReadLine());
+			Console.WriteLine("Multiplying those two numbers results in " + Multiplication(num1, num2));
 		}
+
+		static int Multiplication(int num1, int num2) => num1 * num2;
 	}
 }
